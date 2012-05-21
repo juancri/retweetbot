79162b8d837f5db74c1a6d27fa37f8c5fc601a02
Juan C. Olivares <juancri@juancrimac.(none)>
2012-05-21 19:14:54 -0400

    Updating README to include the patch for native retweet. Adding support for native retweet.

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ friends = [ 1, 2, 3 ] # Add here the IDs of the users who will be retweeted
 hashtag = '#myhashtag' # Any hashtag or magic word that triggers the retweet
 sleep = 60 # Time betweet queries to Twitter
 count = 100 # Amount of tweets per request (max 100)
+nativeRetweet = True # If true, retweets natively. If false, retweets using "RT @user:" 
 
 # API initialization
 # WARNING: Don't share these keys
@@ -51,11 +52,15 @@ while 1:
 			continue
 		
 		# let's retweet
-		retweet = 'RT @' + status.user.screen_name + ": " + status.text
-		if len (retweet) > 140:
-			retweet = retweet [:137] + "..."
-		print "Tweeting:", retweet
-		api.PostUpdate (retweet)
+		if nativeRetweet:
+			print "Retweeting:", status.user.screen_name, status.text
+			api.PostRetweet (status.id)
+		else:
+			retweet = 'RT @' + status.user.screen_name + ": " + status.text
+			if len (retweet) > 140:
+				retweet = retweet [:137] + "..."
+			print "Tweeting:", retweet
+			api.PostUpdate (retweet)
 
         # zZzZzZ
         time.sleep (sleep)
